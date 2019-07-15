e5219247c26411f6ae955331b8306c03adb70cfd
hiteshahuja99 <htshahuja501@gmail.com>
2019-07-15 18:52:06 +0530

    reverse of array using atoi

---
+++ cli.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,const char*argv[])
+{
+	
+	char arr[]={'a','b','c','\0'};
+	char *ptr=arr;
+	printf("%s",ptr);
+}
+
---
+++ reversearr.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, const char *argv[])
+{
+	int arr[argc-1];
+	int j=0;
+	int l=0;
+	int ar[argc-1];
+	for(int i=1;i<argc;i++)
+	{
+		arr[j]=atoi(argv[i]);
+		j++;
+	 printf("%d",arr[j-1]);
+        }
+        
+	for(int k=(sizeof(arr)-1)/4;k>=0;k--)
+
+	{
+		ar[l]=arr[k];
+		l++;
+		printf("%d",arr[k]);
+	}
+	
+	return 0;
+}
+
