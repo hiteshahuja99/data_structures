2146b8da4ae43c47452654cc4bb8bc723ba0b231
hiteshahuja99 <htshahuja501@gmail.com>
2019-08-02 10:23:27 +0530

    Swpstr

--- bashc.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define SHELLSCRIPT " #/bin/bash \n\ read filename\n \ echo \"The $filename is in :-\" \n \ locate $filename\n \ echo \"you are now in this directory :-\" \n \ pwd"
-int main()
-{
-	system(SHELLSCRIPT);
-	return 0;
-}
-
--- swpstr.c
+++ swpstr.c
@@ -1,33 +1,33 @@
 #include<stdio.h>
 int main()
 {
-	int n1=0;
-	scanf("%d",&n1);
-	char s1[n1];
-	char s2[n1];
-	char temp;
-	//int len=(sizeof(s1)/sizeof(s1[0]));
-	//printf("%d",len);
-	for(int i=0;i<n1;i++)
-	{
-	scanf("%s",&s1[i]);
-	}
-	for(int i=0;i<n1;i++)
-	{
-	scanf("%s",&s2[i]);
-	}
-	for(int j=0;j<n1;j++)
-	{
-		temp=s1[j];
-		s1[j]=s2[j];
-		s2[j]=temp;
-	}
-	
-	
-		printf("%s\n",s1);
-		printf("%s",s2);
-	
-	return 0;
+        int n1=0;
+        scanf("%d",&n1);
+        char s1[n1];
+        char s2[n1];
+        char temp;
+        //int len=(sizeof(s1)/sizeof(s1[0]));
+        //printf("%d",len);
+        for(int i=0;i<n1;i++)
+        {
+        scanf("%s",&s1[i]);
+        }
+        for(int i=0;i<n1;i++)
+        {
+        scanf("%s",&s2[i]);
+        }
+        for(int j=0;j<n1;j++)
+        {
+                temp=s1[j];
+                s1[j]=s2[j];
+                s2[j]=temp;
+        }
+
+
+                printf("%s\n",s1);
+                printf("%s",s2);
+
+        return 0;
 
 }
 
