9f8a208b9098171201ab9cbbf6b2d8a8d0308d46
hiteshahuja99 <htshahuja501@gmail.com>
2019-08-09 10:45:11 +0530

    selection sort

--- selsort.c
+++ selsort.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
 int main(int argc,const char*argv[]){
 
-	int temp=0;
+	int temp=0;int position=0;
 	int arr[argc-1];
 	for(int i=1;i<argc;i++)
 	{
 		arr[i-1]=atoi(argv[i]);
 	}
 	for(int i=0;i<argc-1;i++){
+	position=i;
 		for(int j=i+1;j<argc-1;j++){
-			if(arr[i]>arr[j]){
-				temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
+			if(arr[position]>arr[j]){
+				position=j;
 			}
+		}
+		if(position !=i)
+		{
+			temp=arr[i];
+			arr[i]=arr[position];
+			arr[position]=temp;
 		}
 	}
 		for(int i=0;i<argc-1;i++){
